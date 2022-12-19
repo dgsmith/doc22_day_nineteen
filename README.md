57e1026e109df04a48f36c96099b978f582a989a
Grayson Smith <d.grayson.smith@gmail.com>
2022-12-18 21:19:30 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# day_nineteen
+
+A description of this package.
---
+++ Tests/day_nineteenTests/day_nineteenTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import day_nineteen
+
+final class day_nineteenTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(day_nineteen().text, "Hello, World!")
+    }
+}
